86f13416b866c7ae6d99f6c5f027aa458fc8f076
sanderson <caique.borges@al.infnet.edu.br>
2025-09-07 09:34:33 -0300

    Initial commit

---
+++ src/main/java/com/infnet/web_service_company/WebServiceCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.web_service_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceCompanyApplication.class, args);
+	}
+
+}
